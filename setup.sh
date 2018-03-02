9862f5ea3fde2aa0773fb5ba2f415feddc0c3920
esteer <tomdoerr96@gmail.com>
2018-03-02 19:09:53 +0100

    Add linking to neovim

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@
 ln -s ~/git/dotfiles/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 ln -s ~/git/dotfiles/taskrc ~/.taskrc
 ln -s ~/git/dotfiles/vimrc ~/.vimrc
+mkdir ~/.config/nvim
+ln -s ~/git/dotfiles/vimrc ~/.config/nvim/init.vim
 ln -s ~/git/dotfiles/tmux/tmux.conf ~/.tmux.conf
