b9748236062bef05594d811fd04b286fa4a079a2
esteer <tomdoerr96@gmail.com>
2018-03-03 02:59:55 +0100

    Add code from ubuntu bash config

--- bashrc_custom.sh
+++ bashrc_custom.sh
@@ -7,6 +7,22 @@ HISTFILESIZE=2000000
 alias bat="watch 'upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E \"time to empty|percentage\"'"
 alias tw=timew
 
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
 
 # Start tmux by default
 #[[ $TERM != "screen" ]] && exec tmux
