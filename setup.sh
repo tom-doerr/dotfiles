08f89bf135f999341cc12e80b3e28efcb93e06ee
esteer <tomdoerr96@gmail.com>
2018-03-03 02:53:35 +0100

    Functionality to add custom bash config

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+# IPython
 ln -s ~/git/dotfiles/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+
+# Taskwarrior
 ln -s ~/git/dotfiles/taskrc ~/.taskrc
+
+# VIM
 ln -s ~/git/dotfiles/vimrc ~/.vimrc
 mkdir ~/.config/nvim
 ln -s ~/git/dotfiles/vimrc ~/.config/nvim/init.vim
+
+# Tmux
 ln -s ~/git/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+# Bash
+if grep -Fxq "source ~/git/dotfiles/bashrc_custom.sh" ~/.bashrc
+then
+"source ~/git/dotfiles/bashrc_custom.sh" >> ~/.bashrc
+fi
