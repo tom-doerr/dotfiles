2ef1e37d7ef87ced95915779a903a8f515778b19
esteer <tomdoerr96@gmail.com>
2018-03-03 03:22:11 +0100

    Add setup of plugin managers

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ ln -s ~/git/dotfiles/ipython_config.py ~/.ipython/profile_default/ipython_config
 ln -s ~/git/dotfiles/taskrc ~/.taskrc
 
 # VIM
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/git/dotfiles/vimrc ~/.vimrc
 mkdir ~/.config/nvim
 ln -s ~/git/dotfiles/vimrc ~/.config/nvim/init.vim
 
 # Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/git/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # Bash
