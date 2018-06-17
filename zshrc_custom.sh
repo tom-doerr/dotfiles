b5b6cf884a1b6beb39894c85ab184e88a6c3f42b
tom <tom.doerr@tum.de>
2018-06-17 09:15:26 +0200

    Fix task switching

--- zshrc_custom.sh
+++ zshrc_custom.sh
@@ -84,12 +84,16 @@ function ts() {
         task timeboxing_tomorrow
     elif [[ $1 =~ ^-?[0-9]+$ ]]
     then
+        uuid=$(task $1 _uuid)
         stop_taskwarrior_timewarrior
-        task start $1
+        task start $uuid
     else
         stop_taskwarrior_timewarrior
         timew start "$@"
     fi
 }
 
+
 bindkey '^R' history-incremental-search-backward
+
+
