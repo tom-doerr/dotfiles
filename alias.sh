a03412d45ab060770589c52bf2741eb4344aa799
Tom Doerr <tomdoerr96@gmail.com>
2018-08-14 01:42:17 +0200

    Add td alias

--- alias.sh
+++ alias.sh
@@ -12,6 +12,7 @@ alias ta='task add'
 alias tat='task add due:tomorrow'
 alias tatm='task add due:tomorrow+1d'
 alias tb='task add wait:friday scheduled:friday +batch '
+alias td='task done '
 alias bu='task add +bu +clarify +stuff '
 
 alias ng="/usr/local/lib/node_modules/@angular/cli/bin/ng"
