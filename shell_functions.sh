428e65658771e9e622a8d46a99d0895a3afaea9d
Tom Doerr <tomdoerr96@gmail.com>
2018-10-09 21:02:08 +0200

    Update rb with color support

--- shell_functions.sh
+++ shell_functions.sh
@@ -132,7 +132,7 @@ function pa() {
 }
 
 function rb() {
-    tmux split-window -v -t "$pane" "watch -n 0,1 task +bu"
+    tmux split-window -v -t "$pane" "watch --color -n 0,1 task +bu"
 }
 
 function sm() {
