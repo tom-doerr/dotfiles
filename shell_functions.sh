d4dc6ae92c2d3ee718138a4543f2507dd5bccb86
Tom Doerr <tomdoerr96@gmail.com>
2019-01-02 16:36:38 +0100

    Add ds function

--- shell_functions.sh
+++ shell_functions.sh
@@ -184,6 +184,18 @@ function rs() {
     ts $number_top_task_r1_nocolor
 }
 
+function ds() {
+    stop_taskwarrior_timewarrior
+    number_top_task_r1=$(bash -c 'task timeboxing | awk '"'"'NR==3{print $1}'"'"'')    
+    number_top_task_r1_nocolor=$(echo $number_top_task_r1 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+    if [[ $number_top_task_r1_nocolor == '' ]]
+    then 
+        number_top_task_r1=$(bash -c 'task timeboxing | awk '"'"'NR==3{print $2}'"'"'')    
+        number_top_task_r1_nocolor=$(echo $number_top_task_r1 | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+    fi
+    ts $number_top_task_r1_nocolor
+}
+
 function logp() {
 
 
