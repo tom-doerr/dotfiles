d9e40bc1f40b379de3abbe0187480445765b22e8
Tom Doerr <tomdoerr96@gmail.com>
2019-09-18 11:58:54 +0200

    Add basic setup feature

--- setup.sh
+++ setup.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 
-# Basic
-apt install wget
+apt install -y wget python3-pip
 
 # IPython
+pip3 install ipython
 ln -s ~/git/dotfiles/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 
-# Taskwarrior
-ln -s ~/Nextcloud/sonstiges/task ~/.task
-
-if ! grep -Fxq "include ~/git/dotfiles/taskrc_custom" ~/.taskrc
-then
-echo "include ~/git/dotfiles/taskrc_custom" >> ~/.taskrc
-fi
-
-# GTD
-ln -s Nextcloud/documents/gtd/general-references
-ln -s Nextcloud/documents/gtd/incubation/someday-maybe
-ln -s Nextcloud/documents/gtd/projects
-ln -s Nextcloud/documents/gtd/project-support-material
-
-# Timewarrior
-ln -s ~/Nextcloud/sonstiges/timewarrior ~/.timewarrior
-cp ext/on-modify.timewarrior ~/.task/hooks/
-chmod +x ~/.task/hooks/on-modify.timewarrior
+# Inputrc
+ln -s ~/git/dotfiles/inputrc ~/.inputrc
 
 # VIM
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -33,17 +17,17 @@ ln -s ~/git/dotfiles/vimrc ~/.config/nvim/init.vim
 cd ~/.vim/bundle/vimproc.vim && make
 
 # Tmux
-sudo apt-get install xclip
+sudo apt-get install -y tmux, xclip
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/git/dotfiles/tmux/tmux.conf ~/.tmux.conf
-sudo apt-get install fonts-powerline
+sudo apt-get install -y fonts-powerline
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Autojump
-sudo apt install autojump
+sudo apt install -y autojump
 
 # Bash
 if ! grep -Fxq "source ~/git/dotfiles/bashrc_custom.sh" ~/.bashrc
@@ -52,7 +36,7 @@ echo "source ~/git/dotfiles/bashrc_custom.sh" >> ~/.bashrc
 fi
 
 #Zsh
-sudo apt install zsh
+sudo apt install -y zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 if ! grep -Fxq "source ~/git/dotfiles/zshrc_custom.sh" ~/.zshrc
@@ -65,15 +49,35 @@ cd
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-# SSH
-ln -s ~/git/private/ssh_config ~/.ssh/config
-sudo apt-get install keychain
+if [[ "$1" != "basic"]]
+then
+    # Taskwarrior
+    ln -s ~/Nextcloud/sonstiges/task ~/.task
 
-# Inputrc
-ln -s ~/git/dotfiles/inputrc ~/.inputrc
+    if ! grep -Fxq "include ~/git/dotfiles/taskrc_custom" ~/.taskrc
+    then
+    echo "include ~/git/dotfiles/taskrc_custom" >> ~/.taskrc
+    fi
 
-# KDE
-ln -s ~/git/dotfiles/kglobalshortcutsrc ~/.config/klglobalshortcutsrc
+    # GTD
+    ln -s Nextcloud/documents/gtd/general-references
+    ln -s Nextcloud/documents/gtd/incubation/someday-maybe
+    ln -s Nextcloud/documents/gtd/projects
+    ln -s Nextcloud/documents/gtd/project-support-material
 
-# Hueadm
-ln -s ~/git/private/hueadm.json ~/.hueadm.json
+    # Timewarrior
+    ln -s ~/Nextcloud/sonstiges/timewarrior ~/.timewarrior
+    cp ext/on-modify.timewarrior ~/.task/hooks/
+    chmod +x ~/.task/hooks/on-modify.timewarrior
+
+
+    # SSH
+    ln -s ~/git/private/ssh_config ~/.ssh/config
+    sudo apt-get -y install keychain
+
+    # KDE
+    ln -s ~/git/dotfiles/kglobalshortcutsrc ~/.config/klglobalshortcutsrc
+
+    # Hueadm
+    ln -s ~/git/private/hueadm.json ~/.hueadm.json
+fi
