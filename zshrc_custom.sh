b3302a8c607423302a6b9fac8ec16789f8e45925
Tom Doerr <tomdoerr96@gmail.com>
2019-09-18 12:16:37 +0200

    Change theme for other hosts

--- zshrc_custom.sh
+++ zshrc_custom.sh
@@ -7,7 +7,12 @@ export ZSH=$HOME/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+if [[ "$HOST" == "tom-Desktop-18" ]]
+then
+    ZSH_THEME="robbyrussell"
+else
+    ZSH_THEME="agnoster"
+fi
 
 # Set list of themes to load
 # Setting this variable when ZSH_THEME=random
