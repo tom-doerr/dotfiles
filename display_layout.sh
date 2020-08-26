51b31ff7f5f4d69adb2b05c42795d3a6ad84ce99
Tom Dörr <tom.doerr@tum.de>
2020-08-26 21:09:19 +0200

    Add file

---
+++ display_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --primary --mode 1280x1024 --pos 0x336 --rotate normal --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 2560x1440 --pos 1280x128 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-3 --off
