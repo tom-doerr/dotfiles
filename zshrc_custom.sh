f0b6e9d69a85ba154479480ad772526e99c15895
Tom Dörr <tom.doerr@tum.de>
2020-10-11 09:56:31 +0200

    Add warning

--- zshrc_custom.sh
+++ zshrc_custom.sh
@@ -215,3 +215,4 @@ DISABLE_AUTO_UPDATE=true
 
 
 export PYTHONPATH="${PYTHONPATH}:/home/tom/.local/lib/python3.8/site-packages"
+[ -f ~/.zprofile ] || echo "No ~/.zprofile found. You might want to create a symlink using 'ln -s .profile .zprofile'."
