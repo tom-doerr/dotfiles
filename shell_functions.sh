659e63aa82383aa45fd7bc677cec57db0b53731c
Tom Dörr <tom.doerr@tum.de>
2021-01-04 21:53:35 +0100

    Update video time offset

--- shell_functions.sh
+++ shell_functions.sh
@@ -888,7 +888,7 @@ remaining_time() {
         time_obj=$(get_time_h_day obj)
     elif [[ $tag == "video" ]]
     then
-        offset_time=0.25
+        offset_time=3
         time_obj=$(get_time_h_day obj)
     fi
     remaining_break_time_min=$((((($time_obj - $time_to_remove) / $prof_tag_ratio) \
