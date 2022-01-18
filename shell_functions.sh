7bb00fc273fc2868d03b157a7d85cffa80f2bd6d
Tom Dörr <tom.doerr@tum.de>
2022-01-18 01:46:34 +0100

    Add item to clone stats

--- shell_functions.sh
+++ shell_functions.sh
@@ -1115,7 +1115,7 @@ plot() {
 
 get_last_clone_stats() {
     output_metric=$1
-    for e in codex-readme zsh_codex vim_codex TecoGAN DeepSpeech 
+    for e in fix codex-readme zsh_codex vim_codex TecoGAN DeepSpeech 
 do
         echo $e
         gt_output="$(gt $e Nextcloud/documents/github_traffic_stats)"
