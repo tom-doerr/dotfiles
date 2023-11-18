49f2df0aef6817621aded3ae45d4b991debb9911
Tom Dörr <tomdoerr96@gmail.com>
2023-11-18 17:26:10 +0100

    Update shell_settings.sh

--- shell_settings.sh
+++ shell_settings.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-private_key_1=$HOME/.ssh/id_ed25519 
-private_key_2=$HOME/.ssh/id_rsa
-if [[ -f $private_key_1 ]]
-then
-    /usr/bin/keychain --quiet --quick $private_key_1
-fi
+#private_key_1=$HOME/.ssh/id_ed25519 
+#private_key_2=$HOME/.ssh/id_rsa
+#if [[ -f $private_key_1 ]]
+#then
+    #/usr/bin/keychain --quiet --quick $private_key_1
+#fi
 
-if [[ -f $private_key_2 ]]
-then
-    /usr/bin/keychain --quiet --quick $private_key_2
-fi
+#if [[ -f $private_key_2 ]]
+#then
+    #/usr/bin/keychain --quiet --quick $private_key_2
+#fi
 
 
-if [[ $HOST == "" ]]
-then
-    source $HOME/.keychain/$HOSTNAME-sh
-elif [[ $HOSTNAME == "" ]]
-then
-    source $HOME/.keychain/$HOST-sh
-#else
-#    echo 'Error! Can not get hostname, both $HOST and $HOSTNAME are empty.' 1>&2
-#    sleep 0.2
-#    #exit 1
-fi
+#if [[ $HOST == "" ]]
+#then
+    #source $HOME/.keychain/$HOSTNAME-sh
+#elif [[ $HOSTNAME == "" ]]
+#then
+    #source $HOME/.keychain/$HOST-sh
+##else
+##    echo 'Error! Can not get hostname, both $HOST and $HOSTNAME are empty.' 1>&2
+##    sleep 0.2
+##    #exit 1
+#fi
 
 if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
