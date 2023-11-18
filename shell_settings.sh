88d62acc2e2fdace73f07b1662b9032700f5facb
Tom Dörr <tomdoerr96@gmail.com>
2023-11-18 16:11:42 +0100

    Update shell_settings.sh and i3/config, delete kitty.conf, modify picom.conf and vimrc, and add new files .profile, .zprofile, and xremap_config.yml.

--- shell_settings.sh
+++ shell_settings.sh
@@ -4,12 +4,12 @@ private_key_1=$HOME/.ssh/id_ed25519
 private_key_2=$HOME/.ssh/id_rsa
 if [[ -f $private_key_1 ]]
 then
-    /usr/bin/keychain --quiet --quick $private_key_1
+    /usr/bin/keychain --quiet --quick --timeout 1000000000000 $private_key_1
 fi
 
 if [[ -f $private_key_2 ]]
 then
-    /usr/bin/keychain --quiet --quick $private_key_2
+    /usr/bin/keychain --quiet --quick --timeout 1000000000000 $private_key_2
 fi
 
 
