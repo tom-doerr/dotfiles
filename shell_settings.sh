8f5d63ed421118cab17e5fae29b34af3727128e6
Tom Dörr <tomdoerr96@gmail.com>
2023-11-18 17:25:23 +0100

    Update shell settings and configurations

--- shell_settings.sh
+++ shell_settings.sh
@@ -4,12 +4,12 @@ private_key_1=$HOME/.ssh/id_ed25519
 private_key_2=$HOME/.ssh/id_rsa
 if [[ -f $private_key_1 ]]
 then
-    /usr/bin/keychain --quiet --quick --timeout 1000000000000 $private_key_1
+    /usr/bin/keychain --quiet --quick $private_key_1
 fi
 
 if [[ -f $private_key_2 ]]
 then
-    /usr/bin/keychain --quiet --quick --timeout 1000000000000 $private_key_2
+    /usr/bin/keychain --quiet --quick $private_key_2
 fi
 
 
@@ -24,6 +24,15 @@ then
 #    sleep 0.2
 #    #exit 1
 fi
+
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh-agent-thing
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    eval "$(<~/.ssh-agent-thing)"
+fi
+
+
 
 export VISUAL=nvim
 export EDITOR="$VISUAL"
