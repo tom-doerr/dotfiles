c82d625b8db1ab1740b216938f9b082d448d917b
Tom Dörr <tomdoerr96@gmail.com>
2025-06-02 09:09:52 +0200

    Add Neovim init.lua with autosave on InsertLeave and FocusLost

---
+++ .config/nvim/init.lua
@@ -0,0 +1,71 @@
+-- Disable loading of old vim plugins and config to prevent conflicts
+vim.opt.runtimepath:remove(vim.fn.expand('~/.vim'))
+vim.opt.runtimepath:remove('/usr/share/vim/vimfiles')
+vim.g.loaded_vimrc = 1
+vim.g.loaded_gvimrc = 1
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- Import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- Basic Neovim settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+
+-- Autosave on leaving insert mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*", -- Apply to all file types
+  command = "silent! write", -- Silently write the current buffer
+  desc = "Autosave on leaving insert mode"
+})
+
+-- Autosave all modified buffers when Neovim loses focus
+vim.api.nvim_create_autocmd("FocusLost", {
+  pattern = "*", -- Apply to all file types
+  command = "silent! wall", -- 'wall' saves ALL modified buffers
+  desc = "Autosave all modified buffers when Neovim loses focus"
+})
