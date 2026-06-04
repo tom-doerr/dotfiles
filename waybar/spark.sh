9c6af9aa10081de7d77dfa77add45060480a6764
tom-doerr <tomdoerr96@gmail.com>
2026-06-04 21:37:37 +0200

    Show IO pressure in Waybar host status

--- waybar/spark.sh
+++ waybar/spark.sh
@@ -25,14 +25,18 @@ else
 fi
 cat /sys/module/zswap/parameters/enabled 2>/dev/null || echo N
 awk "/Zswap:/{zs=\$2}/Zswapped:/{zw=\$2}END{print zs+0, zw+0}" /proc/meminfo
-awk "NR>1 && \$1 !~ /^\\/dev\\/zram/ {if(\$2==\"partition\"){nvs+=\$3;nv+=\$4}else if(\$2==\"file\"){sfs+=\$3;sf+=\$4}} END{print nv+0, nvs+0, sf+0, sfs+0}" /proc/swaps 2>/dev/null'
+awk "NR>1 && \$1 !~ /^\\/dev\\/zram/ {if(\$2==\"partition\"){nvs+=\$3;nv+=\$4}else if(\$2==\"file\"){sfs+=\$3;sf+=\$4}} END{print nv+0, nvs+0, sf+0, sfs+0}" /proc/swaps 2>/dev/null
+if [ -r /tmp/waybar-nvme-cache-dirty ]; then awk "NR==1{print \$1+0; exit}" /tmp/waybar-nvme-cache-dirty; else echo -1; fi
+awk "/^full /{for(i=1;i<=NF;i++)if(\$i~/^avg60=/){v=\$i; sub(/^avg60=/,0,v); print v+0; found=1}}END{if(!found)print -1}" /proc/pressure/io 2>/dev/null'
 
-# Read cached data (validate 20 fields: g p c m d rx tx pt zd zc zse zs zw nv nvs sf sfs ci ct _)
+# Read cached data (validate 22 fields: g p c m d rx tx pt zd zc zse zs zw nv nvs sf sfs ncd iop ci ct _)
 cached=$(cat "$cache" 2>/dev/null)
 case $(echo "$cached" | wc -w) in
-  20) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs sf sfs pci pct _ <<< "$cached" ;;
-  18) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs pci pct _ <<< "$cached"; sf=0; sfs=0 ;;
-  17) read -r g p c m d prx ptx pt zd zc zse zs zw nv pci pct _ <<< "$cached"; nvs=0; sf=0; sfs=0 ;;
+  22) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs sf sfs ncd iop pci pct _ <<< "$cached" ;;
+  21) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs sf sfs ncd pci pct _ <<< "$cached"; iop=-1 ;;
+  20) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs sf sfs pci pct _ <<< "$cached"; ncd=-1; iop=-1 ;;
+  18) read -r g p c m d prx ptx pt zd zc zse zs zw nv nvs pci pct _ <<< "$cached"; sf=0; sfs=0; ncd=-1; iop=-1 ;;
+  17) read -r g p c m d prx ptx pt zd zc zse zs zw nv pci pct _ <<< "$cached"; nvs=0; sf=0; sfs=0; ncd=-1; iop=-1 ;;
 esac
 prev_rx=$prx; prev_tx=$ptx; prev_pt=$pt
 rate_prx=$prx; rate_ptx=$ptx; rate_dt=1
@@ -50,8 +54,8 @@ else data=$(timeout "$ssh_timeout" ssh -o ConnectTimeout="$connect_timeout" "$ho
 
 # Update cache on success, use cached on failure
 if [[ -n "$data" ]]; then
-  read -r g p ci ct m d rx tx zd zc zse zs zw nv nvs sf sfs <<< "$(echo "$data" | tr ',\n' '  ')"
-  p=${p%.*}; nv=${nv:-0}; nvs=${nvs:-0}; sf=${sf:-0}; sfs=${sfs:-0}
+  read -r g p ci ct m d rx tx zd zc zse zs zw nv nvs sf sfs ncd iop <<< "$(echo "$data" | tr ',\n' '  ')"
+  p=${p%.*}; nv=${nv:-0}; nvs=${nvs:-0}; sf=${sf:-0}; sfs=${sfs:-0}; ncd=${ncd:--1}; iop=${iop:--1}
   rate_prx=${prev_rx:-$rx}; rate_ptx=${prev_tx:-$tx}
   if [[ -n "$prev_pt" ]]; then
     rate_dt=$((now - prev_pt)); [[ $rate_dt -lt 1 ]] && rate_dt=1
@@ -66,7 +70,7 @@ if [[ -n "$data" ]]; then
     fi
   fi
   : ${c:=0}
-  echo "$g $p $c $m $d $rx $tx $now $zd $zc ${zse:-N} ${zs:-0} ${zw:-0} $nv $nvs $sf $sfs $ci $ct _" > "$cache"
+  echo "$g $p $c $m $d $rx $tx $now $zd $zc ${zse:-N} ${zs:-0} ${zw:-0} $nv $nvs $sf $sfs ${ncd:--1} ${iop:--1} $ci $ct _" > "$cache"
   pt=$now; fetch_ok=1
 else
   rx=$prx; tx=$ptx
@@ -96,6 +100,8 @@ gpuv=""; [[ ${g:-0} -ge 0 ]] && gpuv=$(printf "GPU%s%3d%% %3dW" "$(bar $g)" "$g"
 cpuv=$(printf "CPU%s%3d%%" "$(bar $c)" "$c")
 memv=$(pad "$(printf "MEM%s%3d%%" "$(bar $m)" "$m")" 17); [[ $m -gt 95 ]] && memv=$(red "$memv")
 dskv=$(pad "$(printf "DSK%s%3d%%" "$(bar $d)" "$d")" 17); [[ $d -gt 90 ]] && dskv=$(red "$dskv")
+iopv=""; iop_pct=$(awk -v p="${iop:--1}" 'BEGIN{if(p<0)print -1; else printf "%d", p+0.5}')
+if [[ $iop_pct -ge 0 ]]; then iopv=$(printf "%-7s" "IO:${iop_pct}%"); [[ $iop_pct -ge 20 ]] && iopv=$(red "$iopv"); fi
 zram=""; [[ $zc -gt 0 ]] && zram=$(echo "$zd $zc" | awk '{printf "%-15s", sprintf("Z:%.1fG/%.1fx",$1/1073741824,$1/$2)}')
 zswap=""; if [[ "${zse:-N}" == "Y" || ${zs:-0} -gt 0 || ${zw:-0} -gt 0 ]]; then zswap=$(awk -v zs="${zs:-0}" -v zw="${zw:-0}" 'BEGIN{if(zw<=0&&zs<=0)v="ZS:0";else if(zs>0)v=sprintf("ZS:%.1fG/%.1fx",zw/1048576,zw/zs);else v=sprintf("ZS:%.1fG",zw/1048576); printf "%-16s", v}'); fi
 nv=${nv:-0}; nvs=${nvs:-0}; sf=${sf:-0}; sfs=${sfs:-0}
@@ -106,13 +112,17 @@ else
 fi
 nvv=""; if [[ ${shown_nv:-0} -gt 1024 ]]; then nvv=$(awk -v n="$shown_nv" 'BEGIN{if(n>1048576)v=sprintf("NV:%.1fG",n/1048576);else v=sprintf("NV:%dM",n/1024); printf "%-10s", v}'); [[ ${shown_nvs:-0} -gt 0 && $((shown_nv * 100)) -ge $((shown_nvs * 95)) ]] && nvv=$(red "$nvv"); fi
 sfv=""; if [[ ${shown_sf:-0} -gt 1024 ]]; then sfv=$(awk -v n="$shown_sf" 'BEGIN{if(n>1048576)v=sprintf("SF:%.1fG",n/1048576);else v=sprintf("SF:%dM",n/1024); printf "%-10s", v}'); [[ ${shown_sfs:-0} -gt 0 && $((shown_sf * 100)) -ge $((shown_sfs * 95)) ]] && sfv=$(red "$sfv"); fi
+ncdv=""; ncd_pct=$(awk -v p="${ncd:--1}" 'BEGIN{if(p<0)print -1; else printf "%d", p+0.5}')
+if [[ "$host" == "nas" && $ncd_pct -ge 0 ]]; then ncdv=$(printf "%-8s" "NVD:${ncd_pct}%"); [[ $ncd_pct -ge 95 ]] && ncdv=$(red "$ncdv"); fi
 swapv=""
 [[ -n "$zram" ]] && swapv="$zram"
 [[ -n "$zswap" ]] && swapv="${swapv:+$swapv }$zswap"
 [[ -n "$nvv" ]] && swapv="${swapv:+$swapv }$nvv"
 [[ -n "$sfv" ]] && swapv="${swapv:+$swapv }$sfv"
+[[ -n "$ncdv" ]] && swapv="${swapv:+$swapv }$ncdv"
 prefix="$host"
 [[ -n "$gpuv" ]] && prefix="$prefix $gpuv"
 line="$prefix $cpuv $memv"
+[[ -n "$iopv" ]] && line="$line $iopv"
 [[ -n "$swapv" ]] && line="$line $swapv"
 printf "%s %s %s↓ %s↑ %s\n" "$line" "$dskv" "$(fmt $rxs)" "$(fmt $txs)" "$age"
